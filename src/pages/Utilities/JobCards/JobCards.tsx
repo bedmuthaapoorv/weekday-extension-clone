5425ac91c4d2e941f102cfccd0d0814ddff62fd7
Apoorv Bedmutha <apoorvbedmutha@gmail.com>
2024-05-05 15:19:30 +0530

    Added comments to job Cards

--- src/pages/Utilities/JobCards/JobCards.tsx
+++ src/pages/Utilities/JobCards/JobCards.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
 import Utilities from '../Utilities'
 import './JobCards.css'
-import { useRef, useState } from 'react'
+// Flexbox for JobCards, 
 export default function JobCards(props: any) {
-
     return (
         <div className='jobCards' >
             <div className='jobCards__flex' onScroll={(event) => {
                 let obj: any = event.target
+                // requesting a new page, if bottom of the current results is reached
                 if (obj.scrollTop >= obj.scrollHeight - obj.offsetHeight - 10) {
                     props.setOffset(props.offset + 1)
                 }
